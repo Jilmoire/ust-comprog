f849f9e8b581795ead5365193d12428e21b8baf9
jill <guilmarlopez2007@gmail.com>
2026-02-26 22:59:08 +0800

    add math

--- intro-c++/hw3.cpp
+++ intro-c++/hw3.cpp
@@ -1,5 +1,5 @@
 // Date Created: Feb 26, 2026 - 2:11pm
-// Date Updated:
+// Date Updated: Feb 26, 20206 - 10:58pm
 // hw3: intro_c++.financial processing program
 
 #include <iostream>
@@ -8,9 +8,24 @@
 
 using namespace std;
 
+// initialize a math function. the deposit variable will be used for the calculation. currency_type is used to ensure proper conversion.
+float converter(float deposited, string currency_type) {
+    float const PHP_USD = 56.50, PHP_EUR = 61.20, PHP_JPY = 0.38; // fixed exchange rate
+    if (currency_type == "USD") {
+        return deposited / PHP_USD;
+    } 
+    else if (currency_type == "EUR") {
+        return deposited / PHP_EUR;
+    } 
+    else if (currency_type == "JPY") {
+        return deposited / PHP_JPY;
+    } 
+    else {
+        return 0;
+    }
+}
 
 int main(){
-    float const PHP_USD = 56.50, PHP_EUR = 61.20, PHP_JPY = 0.38; // fixed exchange rate
     float deposited = 0;
     string currency_type;
     string CONFIRM_MESSAGE_1 = " is selected. Continue? (Y/N) ";
@@ -37,11 +52,12 @@ int main(){
 
 
     // after confirmation, we will have the currency_type and confirm_1 variables.
-    if(confirm_1 == 'Y'){cout << "Converting PHP to "; cout << currency_type; cout << "\nEnter amount: "; cin >> deposited;} else {cout << "Please try again.";}; // terminates the program if unconfirmed.
-
-    // initialize a math function. the deposit variable will be used for the calculation. currency_type is used to ensure proper conversion.
-
-
+    if(confirm_1 == 'Y'){cout << "Converting PHP to "; cout << currency_type; cout << "\nEnter amount: "; cin >> deposited;} else {cout << "Please try again.";} // terminates the program if unconfirmed.
     
+    // calls the math function above main(). moved the math function outside for simplicity and easier syntax.
+    float converted_amount = converter(deposited, currency_type);
+    cout << "Converted amount: " << converted_amount << endl;
+
+
     return 0;
 }
