3d10d8e461003a86e9b59a86bbf7b0b1582019d2
jill <guilmarlopez2007@gmail.com>
2026-02-26 15:34:35 +0800

    upload

---
+++ README.md
@@ -0,0 +1,8 @@
+## DISCLAIMER:  
+
+**This is a repository for the introduction of computer programming. These are notes and assignments that are ONLY intended as inspiration of your works.**
+
+---
+**Before sourcing, make sure to have distinct modification of the files you intend to use.**  
+If you sourced this repository just add this at the end of the file: *//modified from: github.com/Jilmoire - mm/dd/yy*  
+This repository is Licensed under CC BY-NC-SA 4.0.
---
+++ intro-c++/hw3.cpp
@@ -0,0 +1,24 @@
+// Date Created: Feb 26, 2026 - 2:11pm
+// Date Updated:
+// hw3: intro_c++.financial processing program
+
+#include <iostream>
+// #include <iomanip> //	cout << fixed << setprecision(2);
+
+using namespace std;
+
+
+int main(){
+    float const PHP_USD = 56.50, PHP_EUR = 61.20, PHP_JPY = 0.38; // fixed exchange rate
+    float deposited = 0;
+    int currency_type;
+
+
+    cout << "Currency Converter for Philippine Peso (PHP) to United States Dollar (USD), European Dollar (EUR), and Japanese Yen (JPY).\n" << endl; 
+    cout << "What currency would you like your money to be converted to?\n Available Currencies: USD, EUR, JPY"; cin >> currency_type;
+//  cout << "Amount to be converted: "; cin >> deposited; // user inputs the desired amount
+    
+
+
+    return 0;
+}
